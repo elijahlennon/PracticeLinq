7c4f53c6a51d97293af3dc47eca2e117c370b70e
Elijah Lennon <elijahlennon1@gmail.com>
2022-06-20 14:17:18 -0500

    Comepleted Exercise

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGameNames = new List<string>
+            {
+                "Fallout", "Animal Crossing", "Rocket League", "Legend of Zelda",
+            };
+            
+            //Int32 length = videoGameNames.Count;
+
+            //IEnumerable<string> longvideoGameNames = videoGameNames.Where(name => name.Contains("d"));
+
+            foreach (var i in videoGameNames)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
